308b0b865e4c14e625be4171395eda385b738d6b
nlj <nlj@users.noreply.github.com>
2016-04-01 23:33:11 -0700

    Initial commit with git ignore and CLI life without IO

---
+++ life.hs
@@ -0,0 +1,69 @@
+--This program is free software: you can redistribute it and/or modify
+--it under the terms of the GNU General Public License as published by
+--the Free Software Foundation, either version 3 of the License, or
+--(at your option) any later version.
+--
+--This program is distributed in the hope that it will be useful,
+--but WITHOUT ANY WARRANTY; without even the implied warranty of
+--MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--GNU General Public License for more details.
+--
+--You should have received a copy of the GNU General Public License
+--along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import qualified Data.Map.Strict as Map
+import Data.List (intersperse)
+import Data.List.Split (chunksOf)
+import Control.Concurrent
+
+type Grid = Map.Map (Int, Int) Bool
+
+prettyPrint :: Grid -> String
+prettyPrint g = concat
+    (intersperse "\n" (chunksOf dim (map (\k -> repr (Map.lookup k g)) (Map.keys g))))
+    where dim = maximum (map fst (Map.keys g)) + 1
+          repr (Just True)  = '+'
+          repr (Just False) = ' '
+          repr Nothing      = ' '
+
+dist :: (Int, Int) -> (Int, Int) -> Int
+dist a b = max (abs (fst a - fst b)) (abs (snd a - snd b))
+
+numNeighbors :: (Int, Int) -> Grid -> Int
+numNeighbors cell g = length (Map.filterWithKey (\k v -> (dist k cell == 1) && v) g)
+
+dies :: (Int, Int) -> Grid -> Bool
+dies c g = numNeighbors c g < 2 || numNeighbors c g > 3
+
+spawns :: (Int, Int) -> Grid -> Bool
+spawns c g = numNeighbors c g == 3
+
+evolve :: Grid -> Grid
+evolve g = (foldr (.) id (kill ++ spawn)) g
+    where kill = [(\m -> Map.insert c False m) | c <- (Map.keys g), dies c g]
+          spawn = [(\m -> Map.insert c True m) | c <- (Map.keys g), spawns c g]
+
+spawnCells :: Grid -> Grid
+spawnCells g = (foldr (.) id spawn) g
+    where spawn = [(\m -> Map.insert c True m) | c <- (Map.keys g), spawns c g]
+
+indexedGrid :: Int -> [(Int, Int)]
+indexedGrid dim = [(x, y) | x <- [0..(dim - 1)], y <- [0..(dim - 1)]]
+
+parseGrid :: [String] -> Grid
+parseGrid lines = Map.fromList (zip indices isAlive)
+        where indices = indexedGrid (length $ head lines)
+              isAlive = (map (=='+') (concat lines))
+
+initialGrid = parseGrid ["........",
+                         "........",
+                         "........",
+                         "...+++..",
+                         "....+...",
+                         "........",
+                         "........",
+                         "........"]
+
+main :: IO ()
+main = mapM_ (\b -> (putStrLn b) >> threadDelay 1000000) boards
+        where boards = map prettyPrint ((iterate evolve initialGrid))
